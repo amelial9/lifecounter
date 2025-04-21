1599dff55244f47e311cef8d7dd8ed09db9862f9
Amelia <amelialx99@gmail.com>
2025-04-21 01:29:04 -0700

    player 1

--- lifecounter/ViewController.swift
+++ lifecounter/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var player1Label: UILabel!
+    @IBOutlet weak var player1Lost: UILabel!
+    
+    var player1Life = 20
+    var player2Life = 20
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        player1Lost.isHidden = true
+    }
+    
+    func updateUI() {
+        player1Lost.isHidden = player1Life >= 0
     }
 
-
+    
+    @IBAction func player1Minus1(_ sender: Any) {
+        player1Life = player1Life - 1
+        player1Label.text = "Remaining Life: \(player1Life)"
+        updateUI()
+    }
+    
+    
+    @IBAction func player1Add1(_ sender: Any) {
+        player1Life = player1Life + 1
+        player1Label.text = "Remaining Life: \(player1Life)"
+        updateUI()
+    }
+    
+    
+    @IBAction func player1Minus5(_ sender: Any) {
+        player1Life = player1Life - 5
+        player1Label.text = "Remaining Life: \(player1Life)"
+        updateUI()
+    }
+    
+    
+    @IBAction func player1Add5(_ sender: Any) {
+        player1Life = player1Life + 5
+        player1Label.text = "Remaining Life: \(player1Life)"
+        updateUI()
+    }
+    
 }
-
